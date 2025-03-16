29608451f00c6b1e3fcd7ba2ce4de925f426a004
5zybk1b1lly <paluchszymon236@gmail.com>
2025-03-16 12:39:46 +0100

    Added GUI

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 
-
+void draw_gui();
 
 int main()
 {
@@ -27,7 +27,7 @@ int main()
 
         BeginDrawing();
         ClearBackground(SKYBLUE);
-
+        draw_gui();
 
         EndDrawing();
     }
@@ -35,3 +35,15 @@ int main()
     return 0;
 }
 
+void draw_gui()
+{
+    Rectangle menu {-2, 810, 900, 310};
+    DrawRectangleLinesEx(menu, 4, BLACK);
+
+    DrawText("Atack", 15, 850, 30, BLACK);
+    DrawText("Defend", 15, 900, 30, BLACK);
+    DrawText("Special Atack", 15, 950, 30, BLACK);
+
+    Rectangle console {906, 810, 600, 310};
+    DrawRectangleLinesEx(console, 4, BLACK);
+}

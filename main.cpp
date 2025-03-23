7b481dfa9b24f080ebeaf41e3ff355e6bb5ae3e5
5zybk1b1lly <paluchszymon236@gmail.com>
2025-03-23 13:14:21 +0100

    Upgrade gui

--- main.cpp
+++ main.cpp
@@ -9,13 +9,15 @@
 using namespace std;
 
 
-void draw_gui(int selected);
+void draw_gui(int selected, deque<string> &console_output);
+void update_output_queue(deque<string> &output, string line);
 
 int main()
 {
     const int screenWidth = 1400;
     const int screenHeight = 1000;
     int selected_idx{};
+    deque<string> console_output(5);
     
     InitWindow(screenWidth, screenHeight, "GigantMony");
     //Poks(string name, int hp, int ad, int def, Element _el, const char *path)
@@ -43,7 +45,7 @@ int main()
 
         BeginDrawing();
         ClearBackground(SKYBLUE);
-        draw_gui(selected_idx);
+        draw_gui(selected_idx, console_output);
         player.render(screenWidth / 2 - 520, screenHeight / 2 - 220);
         enemy.render(screenWidth / 2 + 150, screenHeight / 2 - 350);
 
@@ -53,7 +55,7 @@ int main()
     return 0;
 }
 
-void draw_gui(int selected)
+void draw_gui(int selected, deque<string> &console_output)
 {
     Rectangle menu {-2, 810, 900, 310};
     DrawRectangleLinesEx(menu, 4, BLACK);
@@ -67,4 +69,18 @@ void draw_gui(int selected)
 
     Rectangle console {906, 810, 600, 310};
     DrawRectangleLinesEx(console, 4, BLACK);
+    int offset = 0;
+    for (auto line : console_output)
+    {
+        DrawText(line.c_str(), 920, 820 + offset, 20, BLACK);
+        offset += 30;
+    }
+}
+void update_output_queue(deque<string> &output, string line)
+{
+    output.push_back(line);
+    if (output.size() > 5)
+    {
+        output.pop_front();
+    }
 }
